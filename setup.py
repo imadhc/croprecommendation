080c33485f0ec7a241108a18864e2fc0bc1ca377
imadhc <imadhcareem009@gmail.com>
2025-05-07 01:33:34 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str) -> list:
+    """
+    This function will return the list of requirements
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove('-e .')
+    return requirements
+
+
+setup(
+        name='croprecommendation',
+    version='0.0.1',
+    author='Imadh',
+    author_email='imadhcareem009@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+)
+
+
+
